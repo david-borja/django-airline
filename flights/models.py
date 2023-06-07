f68195803e82b1403447eb3357c77bd881ae5cc0
david-borja <davidborjamartinez@gmail.com>
2023-06-07 22:21:53 +0200

    add: passenger model

--- flights/models.py
+++ flights/models.py
@@ -22,3 +22,12 @@ class Flight(models.Model):
 
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
+    
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    # blank=True allows the possibility a passenger has no flights
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+
+    def __str__(self):
+        return f"{self.first} {self.last}"
